0271db8db151bf76a289dfb159f5b35fbce8eb0d
roshi <roshi@hotmail.com>
2023-07-09 01:17:06 +0900

    packer.nvimからlazy.nvimに切り替え

--- init.lua
+++ init.lua
@@ -1,34 +1,30 @@
-vim.cmd.packadd('packer.nvim')
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'AndrewRadev/linediff.vim'
-  use 'dracula/vim'
-  use 'thinca/vim-quickrun'
-  use 'tpope/vim-dadbod'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'nvim-tree/nvim-web-devicons', opt = true}
-  }
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = {'nvim-lua/plenary.nvim'}
-  }
-  use {
-    'nvim-telescope/telescope-file-browser.nvim',
-    requires = {'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim'}
-  }
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/vim-vsnip'
-end)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-
--- backup
--- vim.opt.directory = '~/.cache/nvim'
--- vim.opt.backupdir = '~/.cache/nvim'
--- vim.opt.undodir = '~/.cache/nvim'
+require('lazy').setup({
+  'AndrewRadev/linediff.vim',
+  'dracula/vim',
+  'thinca/vim-quickrun',
+  'tpope/vim-dadbod',
+  {'nvim-lualine/lualine.nvim',dependencies = {'nvim-tree/nvim-web-devicons', opt = true}},
+  {'nvim-telescope/telescope.nvim', tag = '0.1.2', dependencies = {'nvim-lua/plenary.nvim'}},
+  {'nvim-telescope/telescope-file-browser.nvim', dependencies = {'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim'}},
+  'neovim/nvim-lspconfig',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/nvim-cmp',
+  'hrsh7th/cmp-vsnip',
+  'hrsh7th/vim-vsnip',
+})
 
 -- filetype
 vim.api.nvim_create_autocmd('FileType', {
